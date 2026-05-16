34f939d17b68a392f5647891eb3ca2f8cc8e4a22
anonytry <anonnytry@gmail.com>
2026-05-16 13:12:09 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -44,7 +44,7 @@ EXPORTS=(
 )
 
 EXTRA_CMDS=(
-#"echo 'no' | bash <(curl -s https://raw.githubusercontent.com/anonytry/Signify/refs/heads/wip/Signify.sh)"
+"echo 'no' | bash <(curl -s https://raw.githubusercontent.com/anonytry/Signify/refs/heads/wip/Signify.sh)"
 )
 
 
